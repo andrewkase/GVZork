026cb3f25eee5b7f6c7206b0ee76d8182b828e18
andrewkase <158593896+andrewkase@users.noreply.github.com>
2024-11-16 15:33:48 -0500

    Update classes.hpp
    more classes added

--- classes.hpp
+++ classes.hpp
@@ -42,12 +42,67 @@ private:
     std::string description;
     float weight;
     int calories;
+};
 
 
+class NPC {
+public:
+    NPC(std::string n = "", std::string d = "", int m_n = 0, std::vector<std::string> m = {});
+
+    //Getters
+    std::string get_name() const;
+    std::string get_description() const;
+    int get_message_number() const;
+    std::vector<std::string> get_messages() const;
+
+    //Setters
+    void set_name(const std::string& n);
+    void set_description(const std::string& d);
+    void set_message_number(int m_n);
+    void set_messages(const std::vector<std::string>& m);
+
+private:
+    std::string name;
+    std::string description;
+    int message_number;
+    std::vector<std::string> messages;
+};
+
+
+class Location {
+public:
+    Location(std::string n = "", std::string d = "", bool v = false, std::vector<NPC> N = {}, std::vector<Item> I = {});
+
+    //Getters
+    std::string get_name() const;
+    std::string get_description() const;
+    bool is_visited() const;
+    std::vector<NPC> get_room_npcs() const;
+    std::vector<Item> get_room_items() const;
+
+    //Setters
+    void set_name(const std::string& n);
+    void set_description(const std::string& d);
+    void set_visited(bool v);
+    void set_room_npcs(const std::vector<NPC>& N);
+    void set_room_items(const std::vector<Item>& I);
+    void remove_room_item(int i);
+    void print_room() const;
+
+private:
+    std::string name;
+    std::string description;
+    bool visited;
+    std::vector<NPC> room_npcs;
+    std::vector<Item> room_items;
 };
 
 
 
+
+
+
+#endif
 
 
 
